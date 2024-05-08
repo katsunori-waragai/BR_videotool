a63c3ace03460ed6b2c4ceb9aeb27a23776d2323
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-05-08 15:06:02 +0900

    added side_by_side.py (#1)
    * added side_by_side.py
    
    * added side_by_side.py
    
    * added side_by_side.py

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # BR_videotool
 video tool
+
+## tools
+- side_by_side.py 
+```commandline
+python3 side_by_side.py -h
+usage: side_by_side.py [-h] leftname rightname
+
+positional arguments:
+  leftname    videofile for left
+  rightname   videofile for right
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
---
+++ side_by_side.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import cv2
+import argparse
+
+def concatenate_videos(left_video_path, right_video_path, output_path):
+    left_video = cv2.VideoCapture(left_video_path)
+    right_video = cv2.VideoCapture(right_video_path)
+
+    width = int(left_video.get(cv2.CAP_PROP_FRAME_WIDTH) + right_video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = max(int(left_video.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(right_video.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    fps = min(left_video.get(cv2.CAP_PROP_FPS), right_video.get(cv2.CAP_PROP_FPS))
+    frame_size = (width, height)
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_path, fourcc, fps, frame_size)
+
+    while True:
+        ret1, frame_left = left_video.read()
+        ret2, frame_right = right_video.read()
+        if not (ret1 and ret2):
+            break
+
+        concatenated_frame = cv2.hconcat([frame_left, frame_right])
+        out.write(concatenated_frame)
+
+    left_video.release()
+    right_video.release()
+    out.release()
+
+    print("finished concatenate videos.")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("leftname", help="videofile for left")
+    parser.add_argument("rightname", help="videofile for right")
+#    parser.add_argument("--outname", default="side_by_side.mp4", help="output video name")
+    args = parser.parse_args()
+
+    left = Path(args.leftname)
+    right = Path(args.rightname)
+#    outname = Path(args.outname)
+
+    left_video_path = args.leftname
+    right_video_path = args.rightname
+    output_path = "concatenated_video.mp4"
+    concatenate_videos(left_video_path, right_video_path, output_path)
